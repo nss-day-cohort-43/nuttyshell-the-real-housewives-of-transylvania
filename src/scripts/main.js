f8c4bbe8bba9a667a0e4835f9531ca649a515c00
Devco2011 <devco2011@gmail.com>
2020-09-24 13:45:45 -0500

    Login and registration working. Added LoadUI function to main.js to check if user is stored in session storage.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,18 @@ import { Nutshell } from "./Nutshell.js"
     4. Also, if the user authenticates, and the login form is initially shown
         ensure that the Nutshell component gets rendered
 */
+let credentials = {}
+
+const loadUI = () => {
+    if (sessionStorage.getItem("activeUser")) {
+        console.log('got user')
+        credentials = sessionStorage.getItem("activeUser")
+        Nutshell();
+    } else {
+        console.log('no user')
+        LoginForm();
+        RegisterForm()
+    }
+
+}
+loadUI()
